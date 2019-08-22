15c8a0191eeefe2c72dc808301315ca4cfd106ed
toby petty <tobypetty@hotmail.com>
2019-08-21 23:32:37 -0400

    make_transfers function

--- fpl_predictor/strategies/sample.py
+++ fpl_predictor/strategies/sample.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from fpl_predictor.squad_builder import SquadBuilder
+from fpl_predictor.squad import MaxedOutTeam
 
 
 def build(self: SquadBuilder, year: int, week: int, live=False, **kwargs):
@@ -54,15 +55,41 @@ def evaluate_transfers(self, year, week, live=False, percent_chance=100,
     if unavailable_first:
         df.sort_values(by=["first_team_unavailable", "squad_unavailable"], ascending=False, inplace=True)
 
-    # Drop duplicates since players can only come in/out once:
-    df["out_code"] = df["out_code"].astype(str)
-    df = df.drop_duplicates(subset="out_code", keep="first")
-    df["code"] = df["code"].astype(str)
-    df = df.drop_duplicates(subset="code", keep="first")
-
-    # Convert codes back to ints:
-    df["out_code"] = df["out_code"].astype(float).astype(int)
-    df["code"] = df["code"].astype(float).astype(int)
-
     return df.reset_index(drop=True)
 
+
+def make_tranfers(self: SquadBuilder, n_transfers: int,
+                  transfer_list: pd.DataFrame):
+    """Attempt to make the transfers returned by the `evaluate_transfers`
+    function to the currently selected squad.
+
+    Args:
+        self (SquadBuilder): instance of the SquadBuilder class.
+        n_transfers (int): total number of transfers to make to squad. Note it
+            may not be possible to make the total number of transfers.
+        transfer_list (pd.DataFrame): output from the `evaluate_transfers`
+            function.
+    """
+    n_made = 0
+    for t in range(n_transfers):
+        if not len(transfer_list):
+            break
+        for row in transfer_list.iterrows():
+            try:
+                p = row[1]
+                test_squad = self._test_new_squad
+                test_squad.selected = self.squad.selected
+                remove_code = p["out_code"]
+                add_code = p["code"]
+                test_squad.remove_player(remove_code)
+                test_squad.add_player(code=p["code"], name=p["name"], position=p["position"],  value=p["value"],
+                                      score=p["score"], score_per_value=p["score_per_value"], team=p["team"])
+                transfer_list = transfer_list.loc[transfer_list["code"] != add_code]
+                transfer_list = transfer_list.loc[transfer_list["out_code"] != remove_code]
+                self.squad.selected = test_squad.selected
+                n_made += 1
+                break
+            except MaxedOutTeam:
+                continue
+
+    return n_made
